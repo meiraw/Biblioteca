a6f3abcc94beb00949078978b457520210c3b684
Wener Meira <meiradev23@gmail.com>
2026-08-05 17:00:45 -0400

    anotações

--- src/main/java/com/meira/biblioteca_api/Repository/LivroRepository.java
+++ src/main/java/com/meira/biblioteca_api/Repository/LivroRepository.java
@@ -5,11 +5,13 @@ import com.meira.biblioteca_api.Model.LivroModel;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 import java.util.UUID;
-
-public interface LivroRepository extends JpaRepository<LivroModel, UUID> {
+// JpaSpecificationExecutor<LivroModel>  faz consultas dinâmicas
+public interface LivroRepository extends JpaRepository<LivroModel, UUID>, JpaSpecificationExecutor<LivroModel> {
     List<LivroModel> findByTituloContainingIgnoreCase(String titulo);
     Page<LivroModel> findByStatus(StatusLivro status, Pageable pageable);
+
 }
---
+++ src/main/java/com/meira/biblioteca_api/Specification/LivroSpecification.java
@@ -0,0 +1,24 @@
+package com.meira.biblioteca_api.Specification;
+
+import com.meira.biblioteca_api.Model.LivroModel;
+import org.springframework.data.jpa.domain.Specification;
+
+//Responsavel para montar consulta de livro (Filtro )
+//Ele é o where para buscas
+public class LivroSpecification {
+
+
+    //Cria a regra da pesquisa para o livro model
+    //No caso o busca será direta ao titulo do livro com o "String titulo "
+    public static Specification<LivroModel> tituloContem(String titulo) {
+        //Retorna a pesquisa , neste caso dentro do LivroModel
+        return (root, query, criteriaBuilder) ->
+                    criteriaBuilder.like(
+                            criteriaBuilder.lower(root.get("titulo")), // significa pegar o titulo do livro
+                            "%" + titulo.toLowerCase() + "%"            // transformando todos os caracteres em minusculos
+                    );
+
+    }
+}
+
+
